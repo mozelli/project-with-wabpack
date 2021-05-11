21cbb145098d1b52c35a45c9b4bb4a94cd6cb1c3
Joao Mozelli Neto <joaomozelli@hotmail.com>
2021-05-11 15:49:58 -0300

    update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,15 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    mode: 'development',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+      },
 };
