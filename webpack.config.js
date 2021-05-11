b11a81221e55b46d0a462bccb8560c00d02d9f6a
Joao Mozelli Neto <joaomozelli@hotmail.com>
2021-05-11 17:05:08 -0300

    Sanitizing the code

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    //mode: 'production',
     mode: 'development',
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
